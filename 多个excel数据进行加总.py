070aaa1048d598507782cb309f791f653568cf15
farewellfirefly <43577557+farewellfirefly@users.noreply.github.com>
2022-04-18 11:03:10 +0800

    Update 多个excel数据进行加总.py
    修复了读取NaN值会取代有效数据的bug

--- 多个excel数据进行加总.py
+++ 多个excel数据进行加总.py
@@ -10,40 +10,60 @@ import numpy as np
  
 def readname():
     
-    filePath = input('请输入待汇总数据所在的文件夹路径，例如: C:\\Users\\firefly\\Desktop\\数据汇总  \n须保证该文件夹\
-下除待统计的excel文件外，无其它文件或文件夹！ \n文件夹路径:')
-    m = int(input('请输入表格区域的行数：\n'))
-    n = int(input('请输入表格区域的列数：\n'))
-    #获取文件下所有excel文件的名称
+    #filePath = input('请输入待汇总数据所在的文件夹路径，例如: C:\\Users\\firefly\\Desktop\\数据汇总  \n须保证该文件夹\
+#下除待统计的excel文件外，无其它文件或文件夹！ \n文件夹路径:')
+    #m = int(input('请输入表格行数：\n'))
+    #n = int(input('请输入表格列数：\n'))
+
+    filePath = 'C:\\Users\\firefly\\Desktop\\zhigong'
     names = os.listdir(filePath)
+    
+    #自动获取表格活动区域行列数
+    file=os.path.join(filePath,names[0])
+    df = pd.read_excel(file,header=None,keep_default_na=False)
+    m = df.shape[0]
+    n = df.shape[1]
+    
+    print(m,n)
     return names,filePath,m,n
 
 
 def mergeexcel(m,n,names,filePath):
     
-    #创建一个值全部为0的dataframe用来存数
+    #创建一个值为0的dataframe用来存数
     pdd=pd.DataFrame(np.zeros([m,n]))
+    types=set()
     for name in names:
-        #拼接每个excel文件的路径
+        #print(pdd)
         file=os.path.join(filePath,name)
         print('正在合并计算'+str(name))
-        df = pd.read_excel(file,header=None)
+        #读取的时候把NaN值以空值读入，视作无表头
+        df = pd.read_excel(file,header=None,keep_default_na=False)
         for i in range(0,m):
             for j in range(0,n):
-                #如果是文字，则原样复制
-                if type(df.iloc[i,j]) == str:
-                    pdd.iloc[i,j] = df.iloc[i,j]
+
+                #print(pdd)
+                # 如果是空值，则跳过        
+                if df.iloc[i,j] == '':
+                    continue
+                # 如果是文字，则原样复制  
+                elif type(df.iloc[i,j]) == str:
+                    if pdd.iloc[i,j] == 0:
+                        pdd.iloc[i,j] = df.iloc[i,j]
                 #如果是数字，则继续加总
                 elif type(df.iloc[i,j]) == int or float:
                     pdd.iloc[i,j] = pdd.iloc[i,j]+df.iloc[i,j]
                 else:
                     print("报错！表格里有数据既不是str也不是int或float,快debug一下！")
+  
+
     return pdd    
  
  
 if __name__ == "__main__":
     names,filePath,m,n = readname()
     result = mergeexcel(m,n,names,filePath)
+    print(result)
     #指定输出路径，不修改会报错
     with pd.ExcelWriter("C:\\Users\\firefly\\Desktop\\统计结果.xlsx") as writer:
         result.to_excel(writer, sheet_name='result',header=False,index= False)
